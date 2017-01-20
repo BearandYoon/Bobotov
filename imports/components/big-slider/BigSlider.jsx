eb1a9be151ce23cb78889a0023a6ec7a327cc11b
mac <luckysevenbear@gmail.com>
2017-01-20 13:22:48 +0200

    added big-slider

---
+++ imports/components/big-slider/BigSlider.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import { createContainer } from '.meteor/react-.meteor-data';
+import { Listenings } from '../../api/listenings.js';
+import {_} from 'lodash';
+
+import { Dimmer, Loader } from 'semantic-ui-react';
+//Components
+import ListeningPreview from '../listening-preview/ListeningPreview.jsx';
+
+class BigSlider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentWillMount() {
+    Session.set('pageLimit', 9);
+  }
+
+  componentWillUnmount() {
+    Session.set('pageLimit', 9);
+  }
+
+  render() {
+    var sliderSettings = {
+      autoplay: true,
+      dots: false,
+      infinite: true,
+      speed: 500,
+      draggable: false,
+      autoplaySpeed: 5000,
+      pauseOnHover: true,
+      slidesToShow: 1,
+      arrows: false,
+      slidesToScroll: 1
+    };
+    let listenings = this.props.listenings;
+    let loading = this.props.loading;
+    if(loading) {
+      if(listenings.length) {
+        return (
+          <div>
+            <div className="headline">
+              <div className="headline__item">
+                <div className="headline-icon headline-icon--hot">
+                  <div className="headline-icon__icon">
+                    <svg className="ico-hot" role="img">
+                      <use xlinkHref="#ico-hot"></use>
+                    </svg>
+                  </div>
+                  <div className="headline-icon__text">Горячие предложения:</div>
+                </div>
+              </div>
+            </div>
+            <Slider className="slider" ref={c => this.slider = c } {...sliderSettings}>
+              {listenings.map((listening, index) => {
+              return (
+                <div key={"bigSlide-" + index} className="slider__item">
+                  <ListeningPreview
+                    listeningData={listening}
+                    layout="index"
+                    loading={true}
+                  />
+                </div>);
+              })}
+            </Slider>
+          </div>
+        );
+      } else {
+        //if empty
+        return (<div></div>);
+      }
+    } else {
+      //loading
+      return (
+        <div>
+          <div className="headline">
+            <div className="headline__item">
+              <div className="headline-icon headline-icon--hot">
+                <div className="headline-icon__icon">
+                  <svg className="ico-hot" role="img">
+                    <use xlinkHref="#ico-hot"></use>
+                  </svg>
+                </div>
+                <div className="headline-icon__text">Горячие предложения:</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+export default createContainer(({ params }) => {
+  const listeningsSubscription = Meteor.subscribe('listenings.public', {}, {limit: Session.get('pageLimit')});
+  const loading = listeningsSubscription.ready();
+  const listenings = Listenings.find({"listeningTech.bonuses.bonus3": true}, {sort: {"listeningTech.createdAt": -1}}).fetch();
+  return {loading, listenings}
+}, BigSlider);
