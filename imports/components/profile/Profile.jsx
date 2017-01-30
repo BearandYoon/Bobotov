6a2012cabdb2a82ffec2325ca0f72ddd040ee59b
mac <luckysevenbear@gmail.com>
2017-01-30 13:30:29 +0200

    added profile

---
+++ imports/components/profile/Profile.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { createContainer } from '.meteor/react-.meteor-data';
+
+const Profile = props => {
+  if(!props.data) {
+    return ( <div> Loading... </div>);
+  }
+
+  const statuses = [
+    {key: "agency", text: "Агенство недвижимости"},
+    {key: "user", text: "Пользователь"},
+    {key: "realtor", text: "Риэлтор"}
+  ];
+
+  let { userPhoto, userName="Unknown", userDesc="user"} = props.data.profile;
+  userPhoto = userPhoto ? userPhoto : "/img/unknown.jpg";
+  userDesc = statuses.find(el => el.key == userDesc);
+
+  return (
+    <div onClick={props.onClick} className="profile">
+      <div className="profile-img" style={{backgroundImage: `url(${userPhoto})`}}></div>
+      <div className="flex-clear">
+        <div className="profile-name">{userName}</div>
+        <div className="profile-desc">{userDesc.text}</div>
+      </div>
+      <div className="profile-icon"></div>
+    </div>
+  );
+}
+
+export default Profile;
